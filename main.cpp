0146779efdfeb72167dd5d752bbd94ffccd6cf7c
Polystirolium <eahalfpast@gmail.com>
2026-01-13 13:52:23 +0300

    project setting changed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <GLFW/glfw3.h>
 
@@ -7,32 +5,34 @@ int main()
 {
     if (!glfwInit())
     {
-        std::cerr<<"Failed to initialize GLFW\n";
-        return -1;
-    }
-    // 2. Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(800, 600, "My GLFW Window", NULL, NULL);
-    if (!window) {
-        glfwTerminate(); // Window or OpenGL context creation failed
+        std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    // 3. Make the window's context current
+    GLFWwindow *window = glfwCreateWindow(800, 600, "My GLFW Window", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+   
     glfwMakeContextCurrent(window);
 
-    // 4. Loop until the user closes the window
-    while (!glfwWindowShouldClose(window)) {
+    
+    while (!glfwWindowShouldClose(window))
+    {
         // Render here (e.g. clear the screen)
         glClear(GL_COLOR_BUFFER_BIT); // Requires OpenGL headers (like GL/glew.h)
 
-        // Swap front and back buffers
+        
         glfwSwapBuffers(window);
 
-        // Poll for and process events (keyboard, mouse, etc.)
+        // Опрос и обработка событий (клавиатура, мышь и т.д.)
         glfwPollEvents();
     }
 
-    // 5. Terminate GLFW, clearing all previously allocated resources
+    // Завершение работы GLFW, очистив все ранее выделенные ресурсы
     glfwDestroyWindow(window);
     glfwTerminate();
 
